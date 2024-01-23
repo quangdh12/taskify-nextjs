aa2ba85161bebf9fb98f8ebb4d42bd36aa7872f3
quangdh12 <quangdh.t63@gmail.com>
2024-01-23 11:26:08 +0700

    create sidebar

--- app/(platform)/(dashboard)/_components/nav-item.tsx
+++ app/(platform)/(dashboard)/_components/nav-item.tsx
@@ -11,6 +11,7 @@ import React from 'react'
 import { Activity, CreditCard, Layout, Settings } from 'lucide-react'
 import { usePathname, useRouter } from 'next/navigation'
 import { Button } from '@/components/ui/button'
+import { Skeleton } from '@/components/ui/skeleton'
 
 export type Organization = {
 	id: string
@@ -110,4 +111,15 @@ const NavItem = ({
 		</AccordionItem>
 	)
 }
+NavItem.Skeleton = function SkeletonNavItem() {
+	return (
+		<div className='flex items-center gap-x-2'>
+			<div className='w-10 h-10 relative shrink-0'>
+				<Skeleton className='h-full w-full absolute' />
+			</div>
+			<Skeleton className='h-10 w-full' />
+		</div>
+	)
+}
+
 export default NavItem
--- app/(platform)/(dashboard)/_components/sidebar.tsx
+++ app/(platform)/(dashboard)/_components/sidebar.tsx
@@ -4,9 +4,10 @@ import { Accordion } from '@/components/ui/accordion'
 import { Button } from '@/components/ui/button'
 import { Skeleton } from '@/components/ui/skeleton'
 import { useOrganization, useOrganizationList } from '@clerk/nextjs'
-import { Link, Plus } from 'lucide-react'
+import { Plus } from 'lucide-react'
 import { useLocalStorage } from 'usehooks-ts'
 import NavItem, { Organization } from './nav-item'
+import Link from 'next/link'
 
 interface SidebarProps {
 	storageKey?: string
@@ -48,7 +49,15 @@ const Sidebar = ({ storageKey = 't-sidebar-state' }: SidebarProps) => {
 	if (!isLoadedOrg || !isLoadedOrgList || userMemberships.isLoading) {
 		return (
 			<>
-				<Skeleton />
+				<div className='flex items-center justify-between mb-2'>
+					<Skeleton className='h-10 w-[50%]' />
+					<Skeleton className='h-10 w-10' />
+				</div>
+				<div className='space-y-2'>
+					<NavItem.Skeleton />
+					<NavItem.Skeleton />
+					<NavItem.Skeleton />
+				</div>
 			</>
 		)
 	}
